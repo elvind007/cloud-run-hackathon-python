4a96306f08c90dcfd7dbb420b1b1d5ee397f03bf
Elvin Dsouza <emdd007@gmail.com>
2022-11-18 13:50:56 +0530

    edit

--- main.py
+++ main.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import os
+import json
 import logging
 import random
 from flask import Flask, request
@@ -31,11 +32,58 @@ def index():
 @app.route("/", methods=['POST'])
 def move():
     request.get_data()
-    logger.info(request.json)
+    # logger.info(request.json)
     
     # TODO add your implementation here to replace the random response
+    dims = request.json['arena']['dims']
     
-    return moves[random.randrange(len(moves))]
+    my_state = request.json['arena']['state']['https://cloud-run-hackathon-python-z7wwhw7czq-uc.a.run.app']
+    all_state = request.json['arena']['state']
+
+
+    if my_state['x'] == (dims[0] - 1) and my_state['y'] == 0 :
+        for key, value in all_state.items():
+            if key != 'https://cloud-run-hackathon-python-z7wwhw7czq-uc.a.run.app':
+                if my_state['x'] == value['x'] and my_state['y'] >= (value['y'] - 3):
+                    if my_state['direction'] == 'S':
+                        return 'T'
+                    elif my_state['direction'] == 'E' or my_state['direction'] == 'N':
+                        return 'R'
+                    else:
+                        return 'L'
+                elif my_state['x'] <= (value['x'] + 3) and my_state['y'] == value['y']:
+                    if my_state['direction'] == 'W':
+                        return 'T'
+                    elif my_state['direction'] == 'N' or my_state['direction'] == 'E':
+                        return 'L'
+                    else:
+                        return 'R'
+    
+    if my_state['y'] > 0:
+        if my_state['direction'] == 'N' :
+            for key, value in all_state.items():
+                if key != 'https://cloud-run-hackathon-python-z7wwhw7czq-uc.a.run.app':
+                    if my_state['x'] == value['x'] and my_state['y'] <= (value['y'] + 3):
+                        return 'T'
+            return 'F'
+        elif my_state['direction'] == 'E' or my_state['direction'] == 'S':
+            return 'L'
+        elif my_state['direction'] == 'W' :
+            return 'R'
+
+    if  my_state['x'] < (dims[0] - 1) :
+        if my_state['direction'] == 'E' :
+            for key, value in all_state.items():
+                if key != 'https://cloud-run-hackathon-python-z7wwhw7czq-uc.a.run.app':
+                    if my_state['x'] >= (value['x'] - 3) and my_state['y'] == value['y']:
+                        return 'T'
+            return 'F'
+        elif my_state['direction'] == 'W' or my_state['direction'] == 'N':
+            return 'R'
+        elif my_state['direction'] == 'S' :
+            return 'L'
+
+    return 'T'
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
