b317c9af38302ec69bede8aa30dcf615433f222e
Elvin Dsouza <emdd007@gmail.com>
2022-11-18 14:50:16 +0530

    edit

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
-
-# Copyright 2020 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 import logging
 import random
 from flask import Flask, request
+from json_extract import GetValue2
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-moves = ['F', 'T', 'L', 'R']
+moves = ['F','L', 'R', 'F', 'F', 'L', 'R']
 
 @app.route("/", methods=['GET'])
 def index():
@@ -30,70 +16,57 @@ def index():
 
 @app.route("/", methods=['POST'])
 def move():
-    request.get_data()
+    data = request.get_json()
     logger.info(request.json)
+    t_range, d_range, hit = myself(data)
+    response = check(moves, data,t_range, d_range, hit)
+    logger.info(response)
+    return response
 
-    info=request.get_json()
-    self_url=info["_links"]["self"]["href"]
-    x_lim=info["arena"]["dims"][0]
-    y_lim=info["arena"]["dims"][1]
-    x_pos=info["arena"]["state"][self_url]["x"]
-    y_pos=info["arena"]["state"][self_url]["y"]
-    my_dir=info["arena"]["state"][self_url]["direction"]
-    hit=info["arena"]["state"][self_url]["wasHit"]
-    all_x=[]
-    all_y=[]
-    x_dir=[]
-    y_dir=[]
-    for i in info["arena"]["state"].values():
-        if i["x"]==x_pos:
-            if (i["y"]-y_pos)<=3 and (i["y"]-y_pos)>=-3 and i["y"]!=y_pos:
-                #all_x.append( i["x"])
-                all_y.append(i["y"]-y_pos)
-                # y_dir.append(i["direction"])
-        elif i["y"]==y_pos:
-            if (i["x"]-x_pos<=3) and (i["x"]-x_pos)>=-3 and i["x"]!=x_pos:
-                all_x.append(x_pos-i["x"])
-                #all_y.append(i["y"])
-                # x_dir.append(i["direction"])
-    # (x_dir,y_dir)=relative_direction(all_x,all_y,x_dir,y_dir)
-    for i in all_y:
-        if i<0 and my_dir=="N":
-            return moves[1]
-        elif i>0 and my_dir=="S":
-            return moves[1]
-    for i in all_x:
-        if i<0 and my_dir=="E":
-            return moves[1]
-        elif i>0 and my_dir=="W":
-            return moves[1]
-    return moves[can_move(x_pos,y_pos,x_lim-1,y_lim-1,my_dir)]
-    # TODO add your implementation here to replace the random response
-    
-    # return moves[random.randrange(len(moves))]
 
-def can_move(x_pos,y_pos,x_lim,y_lim,dir):
-    if x_pos<=0 and dir=="W":
-        if y_pos!=0:
-            return 3
+def myself(data):
+    records = GetValue2(data)
+    me = records.get_values('https://cloud-run-hackathon-python-1-2mlstgvneq-uc.a.run.app')
+    x= me['x']
+    y= me['y']
+    hit=me['wasHit']
+    if me['direction'] == 'N':
+        target_range= {'x': [x], 'y': (max(0, y-3), max(0, y-2), max(0, y-1))}
+        defend_range = {'direction': 'N', 'x': [x], 'y': [max(0, y+2), max(0, y+1)]}
+        if y == 0:
+            hit=True
+    elif me['direction'] == 'E':
+        target_range= {'x': [max(0,x+3), max(0,x+2), max(0,x+1)], 'y':[y]}
+        defend_range = {'direction': 'E', 'x': [max(0,x-2), max(0,x-1)], 'y':[y]}
+    elif me['direction'] == 'S':
+        target_range= {'x': [x], 'y': (max(0, y+3), max(0, y+2), max(0, y+1))}
+        defend_range = {'direction': 'S', 'x': [x], 'y': [max(0,y-2), max(0,y-1)]}
+    elif me['direction'] == 'W':
+        target_range= {'x': [max(0,x-3), max(0,x-2), max(0,x-1)], 'y':[y]}
+        defend_range = {'direction': 'W', 'x': [max(0,x+2), max(0,x+1)], 'y':[y]}
+        if x == 0:
+            hit=True
+    else:
+        raise ValueError('no data found for your url')
+    return target_range, defend_range, hit
+
+# loop to check participants
+def check(moves, data,target_range, defend_range, hit) :
+    for val in data['arena']['state'].values():
+        if val['x'] in target_range['x'] and val['y'] in target_range['y']:
+            if hit == True:
+                response = 'L'
+                break
+            else:
+                response = 'T'
+                break
+        if val['direction'] == defend_range['direction'] and val['y'] in defend_range['y'] and val['x'] in defend_range['x'] :
+            response = 'L'
+            break
         else:
-            return 2
-    elif x_pos>=x_lim and dir=="E":
-        if y_pos==y_lim:
-            return 2
-        else:
-            return 3
-    elif y_pos<=0 and dir=="N":
-        if x_pos>=x_lim:
-            return 2
-        else:
-            return 3
-    elif y_pos>=y_lim and dir=="S":
-        if x_pos<=0:
-            return 2
-        else:
-            return 3
-    return 0
+            response = moves[random.randrange(len(moves))]
+    return response
+
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
